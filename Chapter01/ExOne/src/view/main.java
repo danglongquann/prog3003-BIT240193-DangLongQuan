092e2f5e20cef2c3142b3d50a97579221ccf400e
LongQuan <danglongquancv@gmail.com>
2026-01-16 22:53:50 +0700

    Create main.java

---
+++ Chapter01/ExOne/src/view/main.java
@@ -0,0 +1,20 @@
+package view;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("===== menu =====\n");
+
+        bai1.run();
+        bai2.run();
+        bai3.run();
+        bai4.run();
+        bai5.run();
+        bai6.run();
+        bai7.run();
+        bai8.run();
+        bai9.run();
+
+        System.out.println("\n===== KET THUC CHUONG TRINH =====");
+    }
+}
+
